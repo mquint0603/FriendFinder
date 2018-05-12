dd8eba4f3dd90631af62a927b3922c48db94e800
Mary Quint <mary.k.quint@gmail.com>
2018-05-11 21:00:20 -0500

    html routes added

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path")
+
+// require("./routes/apiRoutes")(app);
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var htmlRoutes = require("./app/routing/htmlRoutes")(app);
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
