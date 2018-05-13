fed2f2c24fdcb88bb1c3c14be1e9a3d101d34561
Mary Quint <mary.k.quint@gmail.com>
2018-05-12 20:58:25 -0500

    compatibility functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,17 @@
 const friends = [
     {
         name: "Test1",
-        image: "dummy.url",
-        scores: [5, 4, 1, 2, 4, 3, 5, 5, 1, 2]
-    }
+        image: "http://via.placeholder.com/350x350",
+        scores: [5, 5, 5]
+    },
+    {
+        name: "Test2",
+        image: "http://via.placeholder.com/350x350",
+        scores: [1, 1, 1]
+    },
 ]
 
 module.exports = friends;
+
+
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,34 @@ module.exports = function(app) {
         res.json(friendData);
     });
 
-    // If no matching route is found default to home
+
     app.post("/api/friends", function(req, res) {
+        
+        
+        //compare scores with all objects in the friend data array -------------------------------------
+        compatibilityScores = []
+
+        for (k=0; k< friendData.length; k++){
+            let compatScore = 0
+            for (i = 0; i < req.body.scores.length; i++){
+                compatScore += Math.abs(parseInt(friendData[k].scores[i]) - parseInt(req.body.scores[i]))
+            }
+            console.log(compatScore)
+            compatibilityScores.push(compatScore)
+            // incrementer++
+        }
+        console.log(compatibilityScores)
+
+        
+        // find the postion of the most compatible friend in the friend data array _____________________________
+        var lowest = 0;
+        for (let j = 1; j < compatibilityScores.length; j++) {
+             if (compatibilityScores[j] < compatibilityScores[lowest]) lowest = j;
+        }
+        console.log(lowest + " position of most compatible")
+
+
         friendData.push(req.body);
-        res.json(true);
+        res.json(friendData[lowest]);
     });
 }
