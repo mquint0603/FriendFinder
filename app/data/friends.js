814b41b0c70f70fbbc25da72f2b664b267aee0e2
Mary Quint <mary.k.quint@gmail.com>
2018-05-12 21:38:04 -0500

    updated questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 const friends = [
     {
-        name: "Test1",
-        image: "http://via.placeholder.com/350x350",
-        scores: [5, 5, 5]
+        name: "Snape",
+        image: "https://vignette.wikia.nocookie.net/harrypotter/images/6/63/SnapeSeverus.jpg/revision/latest?cb=20160103201919",
+        scores: [2, 5, 5, 4, 1, 4, 4, 5, 2, 4]
     },
     {
-        name: "Test2",
-        image: "http://via.placeholder.com/350x350",
-        scores: [1, 1, 1]
+        name: "Dumbledore",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Harris_Dumbledore_CoS.jpg/200px-Harris_Dumbledore_CoS.jpg",
+        scores: [3, 5, 2, 1, 4, 2, 3, 4, 4, 5]
     },
 ]
 
